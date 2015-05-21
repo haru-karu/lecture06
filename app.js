1aefae9609964912d0cd44c5dff96e9dec0effda
N.Shimizu <chikoski@gmail.com>
2015-05-21 10:08:57 +0900

    可視化まで実装

--- app.js
+++ app.js
@@ -37,6 +37,41 @@ function onEnableLoopButtonClicked(event){
 function draw(){
   ctx.fillStyle = "rgb(0, 0, 0)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  var data = getData();
+  var i = 0;
+  var offset = 5;
+  var width  = (canvas.width - offset * (data.length  + 1)) / data.length;
+  var x = offset;
+  ctx.fillStyle = "rgb(255, 255, 255)";
+  while(i < data.length){
+    var h = data[i] * canvas.height / 2;
+    ctx.fillRect(x, canvas.height / 2 - h, width, h);
+    x = x + width + offset;
+    i = i + 1;
+  }
+}
+
+function createAnalyser(){
+  var player = document.querySelector("audio");
+  var audioContext = new AudioContext();
+  var source = audioContext.createMediaElementSource(player);
+  var analyser = audioContext.createAnalyser();
+  source.connect(audioContext.destination);
+  analyser.fftSize = 256;
+  source.connect(analyser);
+  return analyser;
+}
+
+function getData(){
+  var data = new Float32Array(analyser.fftSize);
+  analyser.getFloatTimeDomainData(data);
+  return data;
+}
+
+function update(){
+  draw();
+  requestAnimationFrame(update);
 }
 
 var skip15secButton = document.querySelector("#skip");
@@ -53,3 +88,6 @@ enableLoopButton.addEventListener("click",  onEnableLoopButtonClicked);
 
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
+var analyser = createAnalyser();
+
+update();
